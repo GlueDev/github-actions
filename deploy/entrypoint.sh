e23bbc61d744ebc437fa0c3cf1da087f684c9293
Diederik van den Burger <diederik@glue.group>
2019-02-01 14:37:06 +0100

    chore: updated CI/CD config

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,6 +3,5 @@
 export APP_ENV=$([[ $GITHUB_REF == *"master" ]] && echo "production" || echo "staging")
 export SHORT_SHA=$(echo $GITHUB_SHA | head -c7)
 
-sh -c "ls -lah $HOME"
 sh -c "docker push $CONTAINER_URL:$SHORT_SHA"
 sh -c "kubectl set image deployment $APP_DEPLOYMENT $CONTAINER_URL:$SHORT_SHA -n $APP_PROJECT-$APP_ENV"
