793c66dfe47b055130722d9eb2b319abb02c275d
Diederik van den Burger <diederik@glue.group>
2019-02-01 20:12:36 +0100

    fix: now pushes multiple tags

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,8 +3,8 @@
 export APP_ENV=$([[ $GITHUB_REF == *"master" ]] && echo "production" || echo "staging")
 export SHORT_SHA=$(echo $GITHUB_SHA | head -c7)
 
-sh -c "docker build \
-    -t $CONTAINER_URL:latest \
+docker pull $CONTAINER_URL:$APP_ENV || true
+docker build \
     -t $CONTAINER_URL:$SHORT_SHA \
     -t $CONTAINER_URL:$APP_ENV \
-    --cache-from $CONTAINER_URL:$APP_ENV ."
+    --cache-from $CONTAINER_URL:$APP_ENV .
--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,5 +3,6 @@
 export APP_ENV=$([[ $GITHUB_REF == *"master" ]] && echo "production" || echo "staging")
 export SHORT_SHA=$(echo $GITHUB_SHA | head -c7)
 
-sh -c "docker push $CONTAINER_URL:$SHORT_SHA"
-sh -c "kubectl set image deployment $APP_DEPLOYMENT $APP_DEPLOYMENT=$CONTAINER_URL:$SHORT_SHA -n $APP_PROJECT-$APP_ENV"
+docker push $CONTAINER_URL:$SHORT_SHA
+docker push $CONTAINER_URL:$APP_ENV
+kubectl set image deployment $APP_DEPLOYMENT $APP_DEPLOYMENT=$CONTAINER_URL:$SHORT_SHA -n $APP_PROJECT-$APP_ENV
